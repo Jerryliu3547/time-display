cd2919d765294b793ddbc1a96733484e33b61d6a
Dianze Liu <114109108+Jerryliu3547@users.noreply.github.com>
2024-06-21 20:30:55 +0000

    add switch to military mode button

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
+let military_mode = false;
+
 function updatetime () {
     const now = dayjs();
-    const time = now.format('HH:mm:ss A')
-    const date = now.format('dddd, MMMM DD YYYY')
+    const time = now.format('h:mm:ss A');
+    const militaryTime  = now.format('HH:mm:ss')
+    const date = now.format('dddd, MMMM DD YYYY');
 
-
-    document.querySelector('.local-time').innerHTML = time;
-    document.querySelector('.local-date').innerHTML = date;
+    if (!military_mode) {
+        document.querySelector('.js-local-time').innerHTML = time;
+        document.querySelector('.js-local-date').innerHTML = date;
+    } else {
+        document.querySelector('.js-local-time').innerHTML = militaryTime;
+        document.querySelector('.js-local-date').innerHTML = date;
+    }
 };
+
+function switchButton () {
+    document.querySelector('.js-switch-button').innerHTML = 
+    '<button> switch to military mode </button>'
+    addEventListener('click', ()=>{
+        military_mode = !military_mode
+    })
+}
+
+switchButton();
 
 updatetime();
 
-setInterval( updatetime, 1000)
+setInterval(updatetime, 1000)
